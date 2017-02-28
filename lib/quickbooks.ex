f6779d8012744a4ad611e31c645fbcb8d26172e5
Leo Nikkilä <hello@lnikki.la>
2017-02-28 10:44:07 +0200

    Initial commit

---
+++ lib/quickbooks.ex
@@ -0,0 +1,18 @@
+defmodule QuickBooks do
+  @moduledoc """
+  Documentation for QuickBooks.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> QuickBooks.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/quickbooks_test.exs
@@ -0,0 +1,8 @@
+defmodule QuickBooksTest do
+  use ExUnit.Case
+  doctest QuickBooks
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
