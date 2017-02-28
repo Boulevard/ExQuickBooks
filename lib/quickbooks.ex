4056ef681fe162ce879701e8290631a96ba0fda2
Leo Nikkilä <hello@lnikki.la>
2017-02-28 11:57:42 +0200

    Configure OAuth credentials

--- lib/quickbooks.ex
+++ lib/quickbooks.ex
@@ -1,18 +1,31 @@
 defmodule QuickBooks do
   @moduledoc """
-  Documentation for QuickBooks.
+  API client for QuickBooks Online.
   """
+
+  @credential_config ~w(
+    consumer_key
+    consumer_secret
+  )a
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> QuickBooks.hello
-      :world
-
+  Returns the configured OAuth credentials.
   """
-  def hello do
-    :world
+  def credentials do
+    for k <- @credential_config do
+      case Application.get_env(:quickbooks, k) do
+        v when is_binary(v) -> {k, v}
+        nil                 -> raise_missing(k)
+        _                   -> raise_invalid(k)
+      end
+    end
+  end
+
+  defp raise_missing(key) do
+    raise "QuickBooks configuration value '#{key}' is required."
+  end
+
+  defp raise_invalid(key) do
+    raise "QuickBooks configuration value '#{key}' is invalid."
   end
 end
--- test/quickbooks_test.exs
+++ test/quickbooks_test.exs
@@ -2,7 +2,27 @@ defmodule QuickBooksTest do
   use ExUnit.Case
   doctest QuickBooks
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "credentials/0 returns the right credentials" do
+    Application.put_env(:quickbooks, :consumer_key, "key")
+    Application.put_env(:quickbooks, :consumer_secret, "secret")
+
+    assert QuickBooks.credentials == [
+      consumer_key: "key",
+      consumer_secret: "secret"
+    ]
+  end
+
+  test "credentials/0 raises for missing credentials" do
+    Application.delete_env(:quickbooks, :consumer_key)
+    Application.delete_env(:quickbooks, :consumer_secret)
+
+    assert_raise RuntimeError, &QuickBooks.credentials/0
+  end
+
+  test "credentials/0 raises for invalid credentials" do
+    Application.put_env(:quickbooks, :consumer_key, 42)
+    Application.put_env(:quickbooks, :consumer_secret, 42)
+
+    assert_raise RuntimeError, &QuickBooks.credentials/0
   end
 end
