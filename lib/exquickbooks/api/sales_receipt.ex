375ca730e3f711062c6e2df6a9d78fa224c17dc2
Sean Stavropoulos <sean.stavro@gmail.com>
2017-09-20 18:55:15 -0700

    Add salesreceipt create and void support

---
+++ lib/exquickbooks/api/sales_receipt.ex
@@ -0,0 +1,52 @@
+defmodule ExQuickBooks.API.SalesReceipt do
+  @moduledoc """
+  Functions for interacting with the SalesReceipt API.
+
+  This module directly implements operations from the official API:
+  <https://developer.intuit.com/docs/api/accounting/salesreceipt>
+  """
+
+  use ExQuickBooks.Endpoint, base_url: ExQuickBooks.accounting_api
+  use ExQuickBooks.Endpoint.JSON
+
+  alias ExQuickBooks.OAuth.AccessToken
+
+  @doc """
+  Creates an Sales Receipt.
+
+  A SalesReceipt object must have at least one line that describes an item
+  and an amount.
+  """
+  @spec create_sales_receipt(AccessToken.t, json_map) ::
+    {:ok, json_map} | {:error, any}
+  def create_sales_receipt(token, sales_receipt) do
+    request(:post, "company/#{token.realm_id}/salesreceipt", sales_receipt)
+    |> sign_request(token)
+    |> send_json_request
+  end
+
+  @doc """
+  Retrieves a Sales Receipt.
+  """
+  @spec read_sales_receipt(AccessToken.t, String.t) ::
+    {:ok, json_map} | {:error, any}
+  def read_sales_receipt(token, sales_receipt_id) do
+    request(:get, "company/#{token.realm_id}/salesreceipt/#{sales_receipt_id}")
+    |> sign_request(token)
+    |> send_json_request
+  end
+
+  @doc """
+  Void a Sales Receipt. Must include Sparse, Id, and SyncToken as
+  sales_receipt attributes.
+  """
+  @spec void_sales_receipt(AccessToken.t, json_map) ::
+    {:ok, json_map} | {:error, any}
+  def void_sales_receipt(token, sales_receipt) do
+    request(:post, "company/#{token.realm_id}/salesreceipt", sales_receipt, nil, params: [
+      {"include", "void"}
+    ])
+    |> sign_request(token)
+    |> send_json_request
+  end
+end
---
+++ test/exquickbooks/api/sales_receipt_test.exs
@@ -0,0 +1,53 @@
+defmodule ExQuickBooks.API.SalesReceiptTest do
+  use ExUnit.Case, async: false
+  use ExQuickBooks.APICase
+
+  alias ExQuickBooks.API.SalesReceipt
+  alias ExQuickBooks.OAuth.AccessToken
+
+  doctest SalesReceipt
+
+  @token %AccessToken{
+    token: "token",
+    token_secret: "secret",
+    realm_id: "realm_id"
+  }
+
+  test "create_sales_receipt/3 creates an sales_receipt" do
+    load_response("sales_receipt/create_sales_receipt.json") |> send_response
+
+    assert {:ok, %{"SalesReceipt" => _}} =
+      SalesReceipt.create_sales_receipt(@token, %{foo: true})
+
+    assert %{body: body} = take_request()
+    assert String.contains?(to_string(body), "foo")
+  end
+
+  test "create_sales_receipt/3 recovers from an error" do
+    load_response("sales_receipt/create_sales_receipt_error.json")
+    |> Map.put(:status_code, 400)
+    |> send_response
+
+    assert {:error, %{"Fault" => _}} =
+      SalesReceipt.create_sales_receipt(@token, %{foo: true})
+  end
+
+  test "void_sales_receipt/3 voids and retrieves an sales_receipt" do
+    load_response("sales_receipt/void_sales_receipt.json") |> send_response
+
+    assert {:ok, %{"SalesReceipt" => _}} =
+      SalesReceipt.void_sales_receipt(@token, %{foo: true})
+
+    assert %{body: body} = take_request()
+    assert String.contains?(to_string(body), "foo")
+  end
+
+  test "void_sales_receipt/3 recovers from an error" do
+    load_response("sales_receipt/void_sales_receipt_error.json")
+    |> Map.put(:status_code, 400)
+    |> send_response
+
+    assert {:error, %{"Fault" => _}} =
+      SalesReceipt.void_sales_receipt(@token, %{foo: true})
+  end
+end
