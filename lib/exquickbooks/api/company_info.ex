48ec3451f5b619d9f6acf19adf32a71f66caae0c
Evan O'Brien <evanbobrien@gmail.com>
2022-03-20 20:06:30 +0000

    Add CompanyInfo.read_company_info/1

---
+++ lib/exquickbooks/api/company_info.ex
@@ -0,0 +1,24 @@
+defmodule ExQuickBooks.API.CompanyInfo do
+  @moduledoc """
+  Functions for interacting with the CompanyInfo API.
+
+  This module directly implements operations from the official API:
+  <https://developer.intuit.com/app/developer/qbo/docs/api/accounting/all-entities/companyinfo>
+  """
+
+  use ExQuickBooks.Endpoint, base_url: ExQuickBooks.accounting_api()
+  use ExQuickBooks.Endpoint.JSON
+
+  alias ExQuickBooks.OAuth.Credentials
+
+  @doc """
+  Retrieves company info.
+  """
+  @spec read_company_info(Credentials.t()) ::
+          {:ok, json_map} | {:error, any}
+  def read_company_info(credentials) do
+    request(:get, "company/#{credentials.realm_id}/companyinfo/#{credentials.realm_id}")
+    |> sign_request(credentials)
+    |> send_json_request()
+  end
+end
