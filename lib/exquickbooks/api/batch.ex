4293c337b269ca307772ba30989d1edbefffdf5f
Evan O'Brien <evanbobrien@gmail.com>
2022-04-14 16:11:49 +0100

    Add `API.Batch.read_batch`
    While the underlying QuickBooks API supports batch create/update
    operations, this function expects all operations to be reads.

---
+++ lib/exquickbooks/api/batch.ex
@@ -0,0 +1,25 @@
+defmodule ExQuickBooks.API.Batch do
+  @moduledoc """
+  Functions for interacting with the Batch API.
+
+  This module directly implements operations from the official API:
+  <https://developer.intuit.com/app/developer/qbo/docs/api/accounting/all-entities/batch>
+  """
+
+  use ExQuickBooks.Endpoint, base_url: ExQuickBooks.accounting_api()
+  use ExQuickBooks.Endpoint.JSON
+
+  alias ExQuickBooks.OAuth.Credentials
+
+  @doc """
+  Performs the given query operations in a single request
+  """
+  @spec read_batch(Credentials.t(), list(%{bId: String.t(), Query: String.t()})) ::
+          {:ok, json_map} | {:error, any}
+  def read_batch(credentials, queries) do
+    credentials
+    |> make_request(:post, "batch", Jason.encode!(%{BatchItemRequest: queries}))
+    |> sign_request(credentials)
+    |> send_json_request()
+  end
+end
